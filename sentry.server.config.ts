0e7cc752c837ce72b814235f7f6e8df147cbe22c
Veremii <valerii@proginmind.io>
2026-03-18 23:15:35 +0100

    feat: setup sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,8 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://417f783c1adee0a41b7a91c3c4ff3cc3@o438670.ingest.us.sentry.io/4511067615985664",
+  environment: process.env.NEXT_PUBLIC_APP_ENV ?? process.env.NODE_ENV,
+  tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1,
+  sendDefaultPii: true,
+});
